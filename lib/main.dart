67b7825cec2f91da7695fbe5fe9b0d4c5899c137
Safwan shaikh <shaikhsafwan040@gmail.com>
2021-08-26 22:36:56 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:tutorials/home_page.dart';
+import 'package:tutorials/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      // ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
