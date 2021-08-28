05f5e664a80135080700cb36946f81e7e91b0a0c
Safwan shaikh <shaikhsafwan040@gmail.com>
2021-08-28 14:29:17 +0530

    Day 9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use
 
 //import 'package:flutter/mate rial.dart';
 import 'package:flutter/material.dart';
 import 'package:tutorials/pages/login_page.dart';
 import 'package:tutorials/utils/routes.dart';
+import 'package:tutorials/widgets/themes.dart';
 import 'pages/home_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -18,16 +19,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-
-        //primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
+      theme: MyTheme.lightTheme(context),
       debugShowCheckedModeBanner: false,
-      // darkTheme: ThemeData(
-      //   brightness: Brightness.dark,
-      // ),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
