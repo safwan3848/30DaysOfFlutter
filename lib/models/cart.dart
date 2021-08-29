4b88ddd058cc634143736eb892167311e344fd61
Safwan shaikh <shaikhsafwan040@gmail.com>
2021-08-29 20:15:52 +0530

    Day 21 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unused_field, unnecessary_getters_setters, unnecessary_null_comparison
+
+import 'package:tutorials/models/catalog.dart';
+
+class CartModel {
+  //catalog filed
+  late CatalogModel _catalog;
+//collection of ids - store ids of each items
+  final List<int> _itemIds = [];
+//get catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  //Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  //get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+//add item
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+//removce item
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -6,43 +6,45 @@ import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-      brightness: Brightness.light,
-      primarySwatch: Colors.deepPurple,
-      cardColor: Colors.white,
-      buttonColor: darkBluishClor,
-      accentColor: darkBluishClor,
-      canvasColor: creamColor,
-      fontFamily: GoogleFonts.poppins().fontFamily,
-      appBarTheme: AppBarTheme(
-        color: Colors.white,
-        elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black),
-        textTheme: Theme.of(context).textTheme.copyWith(
-              headline6:
-                  context.textTheme.headline6!.copyWith(color: Colors.black),
-            ),
-      ));
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepPurple,
+        cardColor: Colors.white,
+        buttonColor: darkBluishColor,
+        accentColor: darkBluishColor,
+        canvasColor: creamColor,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: Theme.of(context).textTheme.copyWith(
+                headline6:
+                    context.textTheme.headline6!.copyWith(color: Colors.black),
+              ),
+        ),
+      );
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
-      brightness: Brightness.dark,
-      fontFamily: GoogleFonts.poppins().fontFamily,
-      cardColor: Colors.black,
-      buttonColor: lightBluishColor,
-      accentColor: Colors.white,
-      canvasColor: darkcreamcolor,
-      appBarTheme: AppBarTheme(
-        color: Colors.black,
-        elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.white),
-        textTheme: Theme.of(context).textTheme.copyWith(
-              headline6:
-                  context.textTheme.headline6!.copyWith(color: Colors.white),
-            ),
-      ));
+        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        cardColor: Colors.black,
+        buttonColor: lightBluishColor,
+        accentColor: Colors.white,
+        canvasColor: darkcreamcolor,
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.white),
+          textTheme: Theme.of(context).textTheme.copyWith(
+                headline6:
+                    context.textTheme.headline6!.copyWith(color: Colors.white),
+              ),
+        ),
+      );
 
   //colors
   static Color creamColor = Color(0xfff5f5f5);
   static Color darkcreamcolor = Vx.gray900;
   static Color lightBluishColor = Vx.indigo500;
-  static Color darkBluishClor = Color(0xff403b58);
+  static Color darkBluishColor = Color(0xff403b58);
 }
