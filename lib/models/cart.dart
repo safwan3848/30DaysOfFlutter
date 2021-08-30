fd676c646767d11ad514fa63eb424976d6f39b4f
Safwan shaikh <shaikhsafwan040@gmail.com>
2021-08-30 15:47:23 +0530

    Day 27 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,15 +1,17 @@
-// ignore_for_file: unused_field, unnecessary_getters_setters, unnecessary_null_comparison, non_constant_identifier_names, override_on_non_overriding_member, prefer_typing_uninitialized_variables
+// ignore_for_file: unnecessary_null_comparison
 
 import 'package:tutorials/core/store.dart';
 import 'package:tutorials/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-  //catalog filed
+  // catalog field
   late CatalogModel _catalog;
-//collection of ids - store ids of each items
+
+  // Collection of IDs - store Ids of each item
   final List<int> _itemIds = [];
-//get catalog
+
+  // Get Catalog
   CatalogModel get catalog => _catalog;
 
   set catalog(CatalogModel newCatalog) {
@@ -17,10 +19,10 @@ class CartModel {
     _catalog = newCatalog;
   }
 
-  //Get items in the cart
+  // Get items in the cart
   List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
 
-  //get total price
+  // Get total price
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
 }
@@ -31,7 +33,6 @@ class AddMutation extends VxMutation<MyStore> {
   AddMutation(this.item);
   @override
   perform() {
-    //var store;
     store!.cart._itemIds.add(item.id);
   }
 }
@@ -42,7 +43,6 @@ class RemoveMutation extends VxMutation<MyStore> {
   RemoveMutation(this.item);
   @override
   perform() {
-    //var store;
     store!.cart._itemIds.remove(item.id);
   }
 }
